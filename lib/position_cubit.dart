840df9fe7150ace357ce06b558c4a4d5be44d718
Iaroshuk Dmitrii <iaroshuk.dmitrii@gmail.com>
2021-12-03 21:19:53 +0300

    Добавлена карта

---
+++ lib/position_cubit.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+
+class PositionCubit extends Cubit<PositionState> {
+  PositionCubit() : super(const PositionInitialState());
+
+  Future<void> getPosition() async {
+    LocationPermission locationPermission = await Geolocator.checkPermission();
+    if (locationPermission != LocationPermission.denied || locationPermission != LocationPermission.deniedForever) {
+      emit(const PositionLoadingState());
+      try {
+        Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high,
+        );
+        emit(PositionLoadedState(latitude: position.latitude, longitude: position.longitude));
+      } catch (e) {
+        print(e.toString());
+        emit(const PositionErrorState());
+      }
+    }
+  }
+}
+
+abstract class PositionState extends Equatable {
+  const PositionState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class PositionInitialState extends PositionState {
+  const PositionInitialState();
+}
+
+class PositionLoadingState extends PositionState {
+  const PositionLoadingState();
+}
+
+class PositionLoadedState extends PositionState {
+  final double latitude;
+  final double longitude;
+
+  const PositionLoadedState({required this.latitude, required this.longitude});
+}
+
+class PositionErrorState extends PositionState {
+  const PositionErrorState();
+}
